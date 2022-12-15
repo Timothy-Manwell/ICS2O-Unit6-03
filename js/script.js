d20a3f586457e83a0aa244043f836c21843b609e
Timothy Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-12-15 14:42:28 -0500

    test

--- js/script.js
+++ js/script.js
@@ -18,9 +18,8 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function start() {
-  const getWeather = async (URLAddress) => {
-    try {
+ const getWeather = async (URLAddress) => {
+    try{
       const result = await fetch(URLAddress)
       const jsonData = await result.json()
       console.log(jsonData)
@@ -30,7 +29,4 @@ function start() {
       console.log(err)
     }
   }
-  getWeather(
-    "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
-  )
-}
+  getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
